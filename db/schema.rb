4f6062c40055e3c675b3cae4ea4451e8194eb259
Ilya <simonewarlet@gmail.com>
2025-01-28 13:19:08 +0200

    Update schema after ticket and excavator created

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_28_092453) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "excavators", force: :cascade do |t|
+    t.bigint "ticket_id", null: false
+    t.string "company_name", null: false
+    t.text "address"
+    t.boolean "crew_on_side", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ticket_id"], name: "index_excavators_on_ticket_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "request_number"
+    t.string "sequence_number"
+    t.string "request_type"
+    t.datetime "response_due_date_time"
+    t.string "primary_service_area_code"
+    t.jsonb "additional_service_area_codes"
+    t.text "well_known_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["request_number"], name: "index_tickets_on_request_number", unique: true
+  end
+
+  add_foreign_key "excavators", "tickets"
 end
