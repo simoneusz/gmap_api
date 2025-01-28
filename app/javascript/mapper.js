734c223b2813d5a9fcbcdee79bd9ed5ab95a1600
Ilya <simonewarlet@gmail.com>
2025-01-28 16:04:56 +0200

    Add js that initializes maps in views with ticket polygon data

---
+++ app/javascript/mapper.js
@@ -0,0 +1,25 @@
+function parsePolygon(polygonString) {
+    return polygonString
+        .replace(/[(POLYGON)]/g,'')
+        .split(',')
+        .map(point => point.split(' ').map(Number));
+}
+
+function init_maps(){
+    let map_containers = document.querySelectorAll('[id^="map-"]')
+    map_containers.forEach(container =>{
+        let polygon = parsePolygon(container.dataset.polygon)
+        console.log(container.id)
+        let map = L.map(container.id).setView([polygon[0][0], polygon[0][1]], 10);
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        L.polygon([polygon]).addTo(map);
+    })
+
+}
+
+
+document.addEventListener("DOMContentLoaded", init_maps);
