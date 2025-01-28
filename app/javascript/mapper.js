d3dc97e919a647af81888f77d3bc1563b763a896
Ilya <simonewarlet@gmail.com>
2025-01-28 16:24:45 +0200

    Add guard condition if no maps in page

--- app/javascript/mapper.js
+++ app/javascript/mapper.js
@@ -7,6 +7,7 @@ function parsePolygon(polygonString) {
 
 function init_maps(){
     let map_containers = document.querySelectorAll('[id^="map-"]')
+    if (!map_containers) return
     map_containers.forEach(container =>{
         let polygon = parsePolygon(container.dataset.polygon)
         console.log(container.id)
