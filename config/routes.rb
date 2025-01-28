cfb141b2862d19588dc03ba1f2bfc2ea3bd1d0f9
Ilya <simonewarlet@gmail.com>
2025-01-28 14:09:32 +0200

    Add api views for ticket and excavator

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :posts
   get 'up' => 'rails/health#show', as: :rails_health_check
   namespace :api do
-    resources :tickets, only: [:create]
+    resources :tickets, only: %i[create index show]
   end
 end
